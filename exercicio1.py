af8d8e7fa6725d59b4a004d80c4cb80fbf2c937e
Bruna Camargo <99073105+lunacamargo@users.noreply.github.com>
2023-03-03 23:01:04 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,28 @@
+print("Oi, tudo bem? Vamos brincar de matemática básica comigo?")
+
+nome = (input("comece me dizendo o seu nome."))
+print("Oi", nome)
+
+print("Vamos começar com uma soma!")
+num1 = int(input("primeiro digite um número."))
+num2 = int (input("agora, digite o número que você quer somar ao primeiro."))
+print("o resultado da soma entre eles é:", num1+num2)
+
+print("Vamos passar para a subtração.")
+num1 = int(input("digite um número por favor."))
+num2 = int(input("agora você vai digitar um número para subtrair ele."))
+print("o resuldado da subtração entre eles é:", num1-num2)
+
+print("Fácil né! que tal tentar uma multiplicação?")
+num1 = int(input("digite um número."))
+num2 = int(input("por que número você quer dividir ele?"))
+print("o resultado da multiplicação entre eles é:", num1*num2)
+
+print("Chegamos na penultima: Divisão.")
+num1 = int(input("digite um número."))
+num2 = int(input("agora digite um número pra que eu possa dividir o primeiro pra você."))
+print(" o resultado da divisão entre eles é", num1/num2)
+
+print("E a, acertei?")
+num = int(input("Por último vou fazer uma difícil, vamos elevar o número que você escolher ao quadrado. Escolhe um numero pra mim."))
+print("o resultado é:", num*num)
